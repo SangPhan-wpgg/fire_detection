69c4677deaa39188089d3db3345714455cc87880
SangPhan-wpgg <phandinhsang2005@gmail.com>
2026-08-18 23:38:07 +0700

    thêm CLI tương thích cho các chức năng

---
+++ scripts/augment_negatives.py
@@ -0,0 +1,5 @@
+from fire_detection.data.augment import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/validate_data.py
@@ -0,0 +1,5 @@
+from fire_detection.data.validation import main
+
+
+if __name__ == "__main__":
+    main()
